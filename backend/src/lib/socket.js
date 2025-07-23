e1aa45728d8397086b18eb82f5b0086aad8a2162
Ishan Tandukar <077bct019.ishan@sagarmatha.edu.np>
2025-07-23 16:57:10 +0545

    mern chat app completed

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+//use to store online users
+const userSocketMap = {}; //{userId: socketId}
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  //io.emit("userConnected", userId); // Notify all clients about the new user
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap)); // Notify all clients about
+  });
+});
+
+export { io, app, server };
