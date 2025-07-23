3aaf34a0b00a4c2c8c5ac102e3c58861f3bfc7d2
Ishan Tandukar <077bct019.ishan@sagarmatha.edu.np>
2025-07-23 20:13:32 +0545

    mern chat app completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -24,6 +24,7 @@ app.use(
   })
 );
 
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
